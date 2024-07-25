91df822cb999b4a948bc7063244c42607151e74d
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-07-25 11:21:52 +0200

    Setup prisma db

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
+import {
+  RegisterLink,
+  LoginLink,
+} from "@kinde-oss/kinde-auth-nextjs/components";
+
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <LoginLink>Sign in</LoginLink>
+      <RegisterLink>Sign up</RegisterLink>
+    </>
+  );
 }
 
