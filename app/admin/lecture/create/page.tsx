c685d6302013710fc41ffa3567c95b7ce61e4928
Mateusz Kliś <76220346+klisiubb@users.noreply.github.com>
2024-07-27 11:16:25 +0200

    Fixed security in client component

--- app/admin/lecture/create/page.tsx
+++ app/admin/lecture/create/page.tsx
@@ -1,9 +1,22 @@
 "use client";
 import React from "react";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 import { CreateForm } from "../../_components/form/create";
 import { createLectureSchema } from "../../_schemas/createLectureSchema";
+import { redirect } from "next/navigation";
 
 export default function Page() {
+  const { isLoading, isAuthenticated, getPermissions } =
+    useKindeBrowserClient();
+  const { permissions } = getPermissions();
+  const isAdmin = permissions ? permissions.includes("admin") : false;
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isAuthenticated === false || isAdmin === false) {
+    return redirect("/");
+  }
+
   return (
     <div className="min-h-[calc(100vh-160px)] p-6 md:p-10  flex items-center justify-center">
       <CreateForm
